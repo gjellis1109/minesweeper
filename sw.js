34b9d4b1a4d661e3262104971a055db007156c6d
gjellis1109 <grantellis81@gmail.com>
2025-08-14 10:56:04 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Minimal cache-first SW for offline
+const CACHE = "msw-shell-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
